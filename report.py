9056aae4d6ab5172bf456c28c0f7e0223b1acede
Aleksey I. Ozimkov <info@7logic.ru>
2020-08-04 21:55:48 +0300

    Important - Fixed a logical error in Metrika request. Earlier versions not working right. Refactored report.py file. Add url source filename in settings.ini

--- report.py
+++ report.py
@@ -7,60 +7,90 @@ import os
 
 SCRIPT_URL = os.path.dirname(os.path.abspath(__file__))
 REQUEST_PREFIX = "https://api-metrika.yandex.net/stat/v1/data"
+PROJECT_NAME = "ExampleProject"
 
 # request fields
 IDS = "?ids="
-METRICS = "&metrics=" 
-FILTERS = "&filters="
-METRIC_PAGEVIEWS = "ym:s:pageviews"
+LIMITS = "&limit=" 
+LIMIT  = "100000"
+METRICS = "&metrics="
+METRIC_PAGEVIEWS = "ym:pv:pageviews"
+DIMENSIONS = "&dimensions="
+DIMENSION_URL = "ym:pv:URL"
 FROM = "&date1="
-TO = "&date2="
+# TO = "&date2="
 
 # report period
-date1 = "2020-07-01"
-date2 = "2020-07-21"
+date1 = "2020-06-01"
+# date2 = "2020-07-31"
 
 # config import
 config = configparser.ConfigParser()
 config.read('settings.ini')
-token = 'OAuth ' + config['Metrika']['metrika_token']
-counter = config['Metrika']['counter']
+token = 'OAuth ' + config[PROJECT_NAME]['metrika_token']
+counter = config[PROJECT_NAME]['counter']
+links_filename = config[PROJECT_NAME]['filename']
 
-def construct_url(url):
-    metric_url = "EXISTS(ym:pv:URL=*'{}')".format(url)
-    return REQUEST_PREFIX + IDS + counter + METRICS + METRIC_PAGEVIEWS + FILTERS + metric_url + FROM + date1 + TO + date2
+def construct_url():
+    return REQUEST_PREFIX + IDS + counter + LIMITS + LIMIT + METRICS + METRIC_PAGEVIEWS + DIMENSIONS + DIMENSION_URL + FROM + date1 # + TO + date2
 
-def make_request(url):
-    headers = {
-        'Authorization': token
-    }
-    test_request = construct_url(url)
-    response = requests.get(test_request, headers = headers)
-    response_json = response.text
+def make_request():
+    status_code = 0
+    while status_code != 200:
+        headers = {
+            'Authorization': token
+        }
+        request_metrics = construct_url()
+        response = requests.get(request_metrics, headers = headers)
+        response_json = response.content
+        status_code = response.status_code
     return response_json
 
-def get_page_views(url):
-    response = make_request(url)
-    parsed_response = json.loads(response)
-    return int(parsed_response["data"][0]["metrics"][0])
+def get_urls_pageviews_dict(data):
+    data_object = data['data']
+    url_pageviews_dict = {}
+    for i in range(len(data_object)):
+        url_pageviews_dict[data_object[i]['dimensions'][0]['name']] = data_object[i]['metrics'][0]
+    return url_pageviews_dict
+
+def get_all_views():
+    response = make_request()
+    ready_data_object = json.loads(response)
+    return get_urls_pageviews_dict(ready_data_object)
 
 def get_list_of_url(filename):
     columns = 'url'
     data = pd.read_csv('{}/{}'.format(SCRIPT_URL, filename), header=None, names=[columns])
+    data.drop_duplicates(subset=columns, keep=False, inplace=True)
     urls = []
     for i in range(0, len(data)):
         urls.append(data.iloc[i][columns])
     return urls
 
-urls = get_list_of_url("test.csv")
-results = []
-for url in urls:
-    results.append(get_page_views(url))
-ready_dict = {'urls': urls, 'results': results}
+def main():
+    print("Loading data form Metrika ...")
+    all_views_dict = get_all_views()
+    print("{} objects loaded".format(len(all_views_dict)))
+    
+    print("Loading urls from file...")
+    urls = get_list_of_url(links_filename)
+    print("{} urls loaded".format(len(urls)))
 
-result_dataframe = pd.DataFrame.from_dict(ready_dict)
+    
+    print("Processing urls ...")
+    results        = []
+    for url in urls:
+        results.append(int(all_views_dict.get(url, 0)))
+    ready_dict = {'urls': urls, 'results': results}
+    print("{} urls processed".format(len(results)))
 
-# export to csv
-export_filename  = '{}/report.csv'.format(SCRIPT_URL)
-result_dataframe.to_csv(export_filename, sep=';', columns=['urls', 'results'], index = False, encoding='utf-8', quotechar='"')
-print("done")
+    result_dataframe = pd.DataFrame.from_dict(ready_dict)
+
+    # export to csv
+    export_filename  = '{}/report-{}'.format(SCRIPT_URL, links_filename)
+    print("Exporting to './report-{} ...".format(links_filename))
+    result_dataframe.to_csv(export_filename, sep=';', columns=['urls', 'results'], index = False, encoding='utf-8', quotechar='"')
+    print("Already done!")
+
+if __name__ == "__main__":
+    main()
